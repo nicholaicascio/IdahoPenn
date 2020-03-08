8f26d0c00d81b9d1e85a572ae3cb613168a189cd
nicholaicascio <lordcohliani@gmail.com>
2020-03-08 14:28:29 -0600

    interviews
    added audio and captions to interviews, and the view controllers to impliment them. added tooltip imnage to hospital ar view

---
+++ IdahoPenn/Tabs/SallyInterviewViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SallyInterviewViewController.swift
+//  IdahoPenn
+//
+//  Created by N Cascio on 3/8/20.
+//  Copyright © 2020 Nicholai Cascio. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class SallyInterviewViewController: UIViewController {
+
+    var player:AVAudioPlayer = AVAudioPlayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        do{
+            let audiopath = Bundle.main.path(forResource: "Sally_Interview", ofType: "mp3")
+            try player = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audiopath!)as URL)
+        }
+        catch{
+            //error
+        }
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func PlayClicked(_ sender: Any) {
+        player.play()
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
